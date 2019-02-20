c1065ddc076703963e5856dcf6ecd409fe5ecc44
yang <1617586444@qq.com>
2019-02-20 11:04:54 +0800

    命令视图之类的一些不重要东西

---
+++ src/views/Detail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>电影详情页
+    <div>当前电影的ID为：{{ id }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    name: String,
+    age: Number,
+    sex: String
+  },
+  created () {
+    // 根据ID去请求数据
+    console.log(this.$route);
+    console.log(this.$router);
+  }
+}
+</script>
